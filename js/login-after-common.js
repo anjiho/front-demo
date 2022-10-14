7416fe9fe7f4f6ba3edebe4db6bc07d71184af93
안지호/플래닛기타 <anjo0080@gmail.com>
2022-10-14 22:37:11 +0900

    공부 파일 추가

---
+++ js/login-after-common.js
@@ -0,0 +1,12 @@
+//============= 로그인이 된  후 사용하는 공통 스크립트 파일 ==========================
+// let localStorageId = getItemWithExpireTime('id');
+// if (!localStorageId) {
+//     //sessionStorage.setItem('isLogout', true);
+//     location.href = '../html/login.html';
+// }
+
+// const sessionMemberId = sessionStorage.getItem('id');
+// const sessionNickName = sessionStorage.getItem('nickName');
+
+// console.log('sessionMemberId', sessionMemberId);
+// console.log('sessionNickName', sessionNickName);
